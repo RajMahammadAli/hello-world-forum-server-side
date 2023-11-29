76f109d1a17874ee541250e4d2d5cd145807f3f6
RajMahammadAli <ali.rc.raj@gmail.com>
2023-11-29 11:26:31 +0600

    post route is setup and post functionalilty is done

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+
 const app = express();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+const cors = require("cors");
+
+//middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9hya0nn.mongodb.net/?retryWrites=true&w=majority;`;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://alircraj:jVUqcOWEmGU0BimI@cluster0.9hya0nn.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,12 +25,33 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("addPostsDB");
+    const userCollection = database.collection("addPosts");
+
+    app.get("/addPosts", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/addPosts", async (req, res) => {
+      const addPosts = req.body;
+      const result = await userCollection.insertOne(addPosts);
+      res.send(result);
+      console.log(addPosts);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
 }
 run().catch(console.dir);
